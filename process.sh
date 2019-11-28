d410e708181a13820d2a1ac22dc068af3dcfd50b
Garrett Brown <themagnificentmrb@gmail.com>
2019-11-27 18:28:39 -0800

    [temp] Add process.sh

---
+++ process.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+./process_game_addons.py \
+    --game-addons-dir=game-addons \
+    --kodi-source-dir=`pwd`/../kodi \
+    --push-branch=testing \
+    --git \
+    --compile \
+    --filter="yabause"
