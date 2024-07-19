19a311d02d4eda222d74e40e0c603f4053af77af
Garrett Brown <themagnificentmrb@gmail.com>
2024-07-18 18:32:17 -0700

    temp-mame

--- process.sh
+++ process.sh
@@ -2,7 +2,7 @@
 ./process_game_addons.py \
     --game-addons-dir=game-addons \
     --kodi-source-dir=`pwd`/../kodi \
-    `#--push-branch=master` \
+    --push-branch=master \
     --git \
     --compile \
-    --filter="yabause"
+    --filter="mame"
